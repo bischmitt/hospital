db3901c6a7f834a0210c4e5a4e52475b4df77502
Bianca Klein Schmitt <bischmitt98@gmail.com>
2021-11-26 19:07:24 -0300

    layout provisorio

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello')
+const express = require('express')
+const app = express()
+const client = require('./conexao')
+const porta = 3000
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({
+    partialsDir:('views/partials/')
+})
+app.engine('handlebars',hbs.engine)
+app.set('view engine','handlebars')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.listen(porta,()=>{
+    console.log('Vamos arrasar neste projeto!')
+})
