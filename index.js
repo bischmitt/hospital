e56a388c4ff477d1314ae1df9fe71ee3424faa63
Bianca Klein Schmitt <bischmitt98@gmail.com>
2021-11-27 20:16:30 -0300

    Banco de dados funcionando.Colecoes criadas

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const client = require('./conexao')
+const dbo = client.db('hospital')
 const porta = 3000
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({
-    partialsDir:('views/partials/')
-})
+    partialsDir: 'views/partials',
+});
 
 app.engine('handlebars',hbs.engine)
 app.set('view engine','handlebars')
@@ -22,9 +23,67 @@ app.get('/cadastrarMedico', (req, res) => {
     res.render('cadastrarMedico')
 })
 
+app.post('/addMedico', (req,res)=>{
+    const novoMedico = {
+        nome: req.body.nomeMedico,
+        cpf: req.body.cpf,
+        celular: req.body.celular,
+        email: req.body.email,
+        endereco: req.body.cpf,
+        especialidade: req.body.especialidade,
+        crm: req.body.crm,
+        salario: req.body.salario,
+        precoConsulta: req.body.precoConsulta
+    }
+    dbo.collection('medicos').insertOne(novoMedico,(erro,resultado)=>{
+        if(erro) throw erro
+        console.log("Um médico foi cadastrado!")
+        res.redirect('/cadastrarMedico')
+    })
+})
+
 app.get('/cadastrarUsuario', (req, res) => {
     res.render('cadastrarUsuario')
 })
+app.post('/addUsuario', (req,res)=>{
+    const novoUsuario = {
+        nome: req.body.nomeUsuario,
+        dataNascimento: req.body.dataNascimento,
+        cpf: req.body.cpf,
+        celular: req.body.celular,
+        email: req.body.email,
+        endereco: req.body.endereco,
+        genero: req.body.genero,
+        senha: req.body.senha
+    }
+    dbo.collection('usuarios').insertOne(novoUsuario,(erro,resultado)=>{
+        if(erro) throw erro
+        console.log("Um usuário foi cadastrado!")
+        res.redirect('/')
+    })
+})
+
+app.get('/cadastrarEspecialidade', (req,res)=>{
+    res.render('cadastrarEspecialidade')
+})
+app.post('/addEspecialidade',(req,res)=>{
+    const novaEspecialidade = {
+        especialidade: req.body.especialidade,
+    }
+    dbo.collection('especialidades').insertOne(novaEspecialidade, (erro,resultado)=>{
+        if(erro)throw erro
+        console.log('Uma especialidade foi cadastrada!')
+    })
+})
+
+app.get('/login', (req,res)=>{
+    res.render('login')
+})
+
+app.get('/listarMedico', (req,res)=>{
+    res.render('listarMedico')
+})
+
 
 app.listen(porta,()=>{
     console.log('Vamos arrasar neste projeto!')
