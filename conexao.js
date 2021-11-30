98950fe126b8fd3e48fa0ad33bd412f264cf09af
AndresaTrentini <84751015+AndresaTrentini@users.noreply.github.com>
2021-11-30 20:27:33 -0300

    Revert "teste"

---
+++ conexao.js
@@ -0,0 +1,14 @@
+const mongodb = require('mongodb').MongoClient
+const url = 'mongodb+srv://bianca:bianca98@cluster0.jnnvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const client = new mongodb(url)
+
+async function conectar() {
+    try {
+        await client.connect()
+        console.log('Conectada ao Banco de dados')
+    } catch (erro) {
+        console.log('Infelizmente algo deu errado. Tente novamente')
+    }
+}
+conectar()
+module.exports = client
